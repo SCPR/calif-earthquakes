2cc2a9147c979cc518e65f50429a7ab9159337b2
Chris Keller <christopherlawrencekeller@gmail.com>
2013-12-13 12:03:56 -0800

    skips out on creating class instances of earthquake, writing to list and deconstructing classes to write to database. Will query api, add instance to list, take each in a list and write to db

--- manage.py
+++ manage.py
@@ -11,29 +11,6 @@ logging.basicConfig(format='\033[1;36m%(levelname)s:\033[0;37m %(message)s', lev
 
 manager = Manager(app)
 
-class a_single_earthquake():
-    ''' describes and gives structure to a single earthquake '''
-    def __init__(self, id, mag, place, title, date_time, updated, tz, url, felt, cdi, mmi, alert, status, tsunami, sig, resource_type, latitude, longitude, depth):
-        self.primary_id = id
-        self.mag = mag
-        self.place = place
-        self.title = title
-        self.date_time = date_time
-        self.updated = updated
-        self.tz = tz
-        self.url = url
-        self.felt = felt
-        self.cdi = cdi
-        self.mmi = mmi
-        self.alert = alert
-        self.status = status
-        self.tsunami = tsunami
-        self.sig = sig
-        self.resource_type = resource_type
-        self.latitude = latitude
-        self.longitude = longitude
-        self.depth = depth
-
 class UsgsApiQuery(Command):
     "mimics django's get or create function"
     def get_or_create(self, session, model, **kwargs):
@@ -52,7 +29,7 @@ class UsgsApiQuery(Command):
         usgs_api_data = usgs_query_api.json()
         list_of_details_urls = []
         for item in usgs_api_data['features']:
-            if 'Indonesia' in item['properties']['place']:
+            if 'Oklahoma' in item['properties']['place']:
                 usgs_details_link = str(item['properties']['detail'])
                 list_of_details_urls.append(usgs_details_link)
             else:
@@ -67,59 +44,31 @@ class UsgsApiQuery(Command):
             usgs_query_details = requests.get(detail_url, headers=app_config.config_settings['headers'])
             usgs_api_details = usgs_query_details.json()
             list_of_details_data.append(usgs_api_details)
-        self.create_earthquake_classes_from(list_of_details_data)
-
-    "take data and create a list of class instances"
-    def create_earthquake_classes_from(self, list_of_details_data):
-        list_of_earthquake_instances = []
-        for index, item in enumerate(list_of_details_data):
-            this_earthquake = a_single_earthquake(
-                index,
-                item['properties']['mag'],
-                item['properties']['place'],
-                item['properties']['title'],
-                item['properties']['time'],
-                item['properties']['updated'],
-                item['properties']['tz'],
-                item['properties']['url'],
-                item['properties']['felt'],
-                item['properties']['cdi'],
-                item['properties']['mmi'],
-                item['properties']['alert'],
-                item['properties']['status'],
-                item['properties']['tsunami'],
-                item['properties']['sig'],
-                item['properties']['type'],
-                item['geometry']['coordinates'][1],
-                item['geometry']['coordinates'][0],
-                item['geometry']['coordinates'][2],
-            )
-            list_of_earthquake_instances.append(this_earthquake)
-        self.write(list_of_earthquake_instances)
+        self.write(list_of_details_data)
 
     "write class instances to the database"
     def write(self, list_of_earthquake_instances):
         for item in list_of_earthquake_instances:
             thisQuake = self.get_or_create(db_session, Earthquake,
-                primary_id = item.primary_id,
-                mag = item.mag,
-                place = item.place,
-                title = item.title,
-                date_time = item.date_time,
-                updated = item.updated,
-                tz = item.tz,
-                url = item.url,
-                felt = item.felt,
-                cdi = item.cdi,
-                mmi = item.mmi,
-                alert = item.alert,
-                status = item.status,
-                tsunami = item.tsunami,
-                sig = item.sig,
-                resource_type = item.resource_type,
-                latitude = item.latitude,
-                longitude = item.longitude,
-                depth = item.depth
+                primary_id = 2,
+                mag = item['properties']['mag'],
+                place = item['properties']['place'],
+                title = item['properties']['title'],
+                date_time = item['properties']['time'],
+                updated = item['properties']['updated'],
+                tz = item['properties']['tz'],
+                url = item['properties']['url'],
+                felt = item['properties']['felt'],
+                cdi = item['properties']['cdi'],
+                mmi = item['properties']['mmi'],
+                alert = item['properties']['alert'],
+                status = item['properties']['status'],
+                tsunami = item['properties']['tsunami'],
+                sig = item['properties']['sig'],
+                resource_type = item['properties']['type'],
+                latitude = item['geometry']['coordinates'][1],
+                longitude = item['geometry']['coordinates'][0],
+                depth = item['geometry']['coordinates'][2]
             )
 
 class InitDb(Command):
