4abfa4f520bb2e52fa6f364e586854f4544403cc
Chris Keller <christopherlawrencekeller@gmail.com>
2013-12-12 22:29:24 -0800

    moves project to more modular directory and file structure

--- static/scripts/app.js
+++
@@ -1,9 +0,0 @@
-/*
-$(document).ready(function(){
-	$.getJSON($SCRIPT_ROOT + '/return_data_from_database', {
-	}, function(data) {
-		console.log(data);
-	});
-    return false;
-});
-*/
--- template_filters.py
+++
@@ -1,9 +0,0 @@
-import os, datetime, logging
-
-logging.basicConfig(format='\033[1;36m%(levelname)s:\033[0;37m %(message)s', level=logging.DEBUG)
-
-def datetime_format(value):
-	date_format = '%B %d, %Y %I:%M %p'
-	value = datetime.datetime.fromtimestamp(value / 1e3)
-	value = value.strftime(date_format)
-	return value
