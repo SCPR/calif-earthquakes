09f8ddb146dd5db445f7779afc36733f21587b8d
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-08 17:25:00 -0800

    removes whitespacE

--- settings_production.py
+++ settings_production.py
@@ -1,7 +1,6 @@
 APPLICATION_DEBUG       = False
 ASSET_PIPELINE_DEBUG    = False
 ASSETS_DEBUG            = True
-
 SITE_RELATIVE_ROOT      = "/earthquakes/"
 
 execfile('/web/archive/apps/earthquakes/.production_settings')
