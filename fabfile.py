f3dac22466cb9e4d6e5558723d9c4358632cb1a2
Bryan Ricker <bricker88@gmail.com>
2014-01-08 16:44:34 -0800

    Add deploy tasks to fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,6 +6,43 @@ from fabric.api import *
 from fabric.contrib.console import confirm
 from fabric.colors import green
 
+env.user            = 'archive'
+env.hosts           = ['66.226.4.228']
+env.project_root    = '/web/archive/apps/earthquakes/src'
+env.python_exe      = "/web/archive/apps/earthquakes/virtualenvs/earthquakes/bin/python"
+
+def update_code():
+    """
+    Production function to update the code on the remote server
+    """
+    with cd(env.project_root):
+        run('git pull')
+
+def restart():
+    """
+    Production function to restart the server
+    """
+    with cd(env.project_root):
+        run('mkdir -p tmp/ && touch tmp/restart.txt')
+
+def deploy():
+    """
+    Production function to pull the latest code from source control & restarts the server
+    """
+    with cd(env.project_root):
+        update_code()
+        restart()
+
+def revert():
+    """
+    Production function to revert git via reset --hard @{1}
+    """
+    with cd(env.project_root):
+        run('git reset --hard @{1}')
+        restart()
+
+
+
 def localrun():
     ''' run the flask application '''
     local('python runserver.py')
