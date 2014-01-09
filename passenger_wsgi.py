0cf4d9e95f6b02555030101b715a16e081eeec05
Bryan Ricker <bricker88@gmail.com>
2014-01-09 11:53:01 -0800

    Add minified assets to gitignore

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,10 @@
 import sys, os
 
-remote_app_root = "/web/archive/apps/earthquakes"
-INTERP = os.path.join(remote_app_root, 'virtualenvs', 'earthquakes', 'bin', 'python')
+# remote_app_root = "/web/archive/apps/earthquakes"
+# INTERP = os.path.join(remote_app_root, 'virtualenvs', 'earthquakes', 'bin', 'python')
 
-# remote_app_root = "/Users/bryan/projects/calif-earthquakes"
-# INTERP = "/Users/bryan/.virtualenvs/calif-earthquakes/bin/python"
+remote_app_root = "/Users/bryan/projects/calif-earthquakes"
+INTERP = "/Users/bryan/.virtualenvs/calif-earthquakes/bin/python"
 
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
