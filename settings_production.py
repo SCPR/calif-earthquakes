555d74863789515449c153eabd1c1f3548d5b688
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-09 10:16:43 -0800

    removes unneeded setting from config because it was a word I made up

--- settings_production.py
+++ settings_production.py
@@ -1,6 +1,5 @@
-APPLICATION_DEBUG       = False
-ASSET_PIPELINE_DEBUG    = False
-ASSETS_DEBUG            = True
-SITE_RELATIVE_ROOT      = "/earthquakes/"
+DEBUG = False
+ASSETS_DEBUG = True
+SITE_RELATIVE_ROOT = "/earthquakes/"
 
 execfile('/web/archive/apps/earthquakes/.production_settings')
