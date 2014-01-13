ba7cedaa62f3aba1101eb31e01a42774d3e514cd
Bryan Ricker <bricker88@gmail.com>
2014-01-12 17:28:07 -0800

    Remove trailing slash from SITE_RELATIVE_ROOT

--- earthquakes/static/scripts/collections/earthquakes.js
+++ earthquakes/static/scripts/collections/earthquakes.js
@@ -1,6 +1,6 @@
 App.Collections.Earthquakes = Backbone.Collection.extend({
     model: App.Models.Earthquake,
-    url: SITE_RELATIVE_ROOT + 'api/earthquake',
+    url: SITE_RELATIVE_ROOT + '/api/earthquake',
     parse: function(response){
         return response.objects;
     }
