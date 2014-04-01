490c61b582d1c9cfb39033e6ea20f2f151138dbc
Chris Keller <christopherlawrencekeller@gmail.com>
2014-03-31 17:20:52 -0700

    adds date limiter to the api powering the la habra earthquakes map

--- earthquakes/static/scripts/collections/la-habra-earthquakes.js
+++ earthquakes/static/scripts/collections/la-habra-earthquakes.js
@@ -1,6 +1,6 @@
 App.Collections.Earthquakes = Backbone.Collection.extend({
     model: App.Models.Earthquake,
-    url: SITE_RELATIVE_ROOT + '/api/earthquake?q={"filters":[{"name":"date_time_raw","op":"gte","val":"1396048252200"}],"order_by":[{"field":"date_time_raw","direction":"desc"}]}',
+    url: SITE_RELATIVE_ROOT + '/api/earthquake?q={"filters":[{"name":"date_time_raw","op":"gte","val":"1396048252200"},{"name":"date_time_raw","op":"lte","val":"1396224744200"}],"order_by":[{"field":"date_time_raw","direction":"desc"}]}',
     parse: function(response){
         return response.objects;
     }
